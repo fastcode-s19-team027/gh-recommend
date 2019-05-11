64367a1ea816465305f2968248268a98e5ecacb2
HUANGjz <14307130264@fudan.edu.cn>
2019-05-11 14:53:41 -0700

    Modify input and output file path

--- src/main/scala/fastcode/s19/team027/GitHubRecommendation.scala
+++ src/main/scala/fastcode/s19/team027/GitHubRecommendation.scala
@@ -21,9 +21,10 @@ object GitHubRecommendation {
 
     val fileName = if (args.length > 0) args(0) else ""
 
-     val inputData = spark.read.format("json").load(s"s3a://ph.fastcode.s19.github-data/$fileName")
-    //var inputData = spark.read.format("json").load(s"github-data/$fileName")
+    val inputData = spark.read.format("json").load(s"s3a://ph.fastcode.s19.github-data/$fileName")
+    // var inputData = spark.read.format("json").load(s"github-data/$fileName")
     // inputData.take(10).foreach(println)
+    println("Converting data...")
     var rddData = inputData.rdd.map(r => ((r.getAs[String]("user"), r.getAs[String]("repo")), computeScore(r.getAs[String]("type"))))
     
     /* rddData ((user, repo) => score) */
@@ -32,7 +33,7 @@ object GitHubRecommendation {
     /* user (user => [(repo, score)]) */
     var userData = rddData.map(x => (x._1._1, (x._1._2, x._2)))
     var user = userData.groupByKey()
-    
+    println("Computing Similarity...")
     /* similarity (repo1 => (repo2, similarityScore)) */
     val simData = user.flatMap {
     	x => val temp = x._2
@@ -42,6 +43,7 @@ object GitHubRecommendation {
     }.reduceByKey(_+_)
     var similarity = simData.map(x => (x._1._1, (x._1._2, x._2))).groupByKey()
     var repoData = rddData.map(x => (x._1._2, (x._1._1, x._2)))
+    println("Computing Recommendation Score")
     /* result ((user, repo) => recommendationScore) */
     var recData = repoData.join(similarity)
     var rec = recData.flatMap {
@@ -51,6 +53,7 @@ object GitHubRecommendation {
     }
     var result = rec.reduceByKey(_+_)
     var userResult = result.map(x => (x._1._1, (x._1._2,x._2))).groupByKey()
-    userResult.take(10).foreach(println)
+    // userResult.take(10).foreach(println)
+    userResult.map(x => (x._1,x._2.toArray.sortWith(_._2 > _._2).mkString(" "))).saveAsTextFile("s3a://ph.fastcode.s19.gh-output/result")
   }
 }
